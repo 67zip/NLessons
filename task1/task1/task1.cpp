8df11a9c20bf8cb30f3d2de277055ce401a5d811
PENIS <haduevamir013@gmail.com>
2023-11-10 18:28:07 +0300

    Innitial commit

---
+++ task1/task1/task1.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+
+class Calculator {
+private:
+    double num1;
+    double num2;
+
+public:
+    double add() {
+        return num1 + num2;
+    }
+
+    double multiply() {
+        return num1 * num2;
+    }
+
+    double subtract_1_2() {
+        return num1 - num2;
+    }
+
+    double subtract_2_1() {
+        return num2 - num1;
+    }
+
+    double divide_1_2() {
+        if (num2 != 0) {
+            return num1 / num2;
+        }
+        else {
+            std::cout << "„Ґ«Ґ­ЁҐ ­  ­®«м" << std::endl;
+            return 0.0;
+        }
+    }
+
+    double divide_2_1() {
+        if (num1 != 0) {
+            return num2 / num1;
+        }
+        else {
+            std::cout << "„Ґ«Ґ­ЁҐ ­  ­®«м" << std::endl;
+            return 0.0;
+        }
+    }
+
+    bool set_num1(double num) {
+        if (num != 0) {
+            num1 = num;
+            return true;
+        }
+        else {
+            std::cout << "—Ёб«® ­Ґ ¤®«¦­® Ўлвм а ў­® ­г«о" << std::endl;
+            return false;
+        }
+    }
+
+    bool set_num2(double num) {
+        if (num != 0) {
+            num2 = num;
+            return true;
+        }
+        else {
+            std::cout << "—Ёб«® ­Ґ ¤®«¦­® Ўлвм а ў­® ­г«о" << std::endl;
+            return false;
+        }
+    }
+};
+
+int main() {
+    Calculator calculator;
+    double num1, num2;
+
+    while (true) {
+        std::cout << "‚ўҐ¤ЁвҐ num1: ";
+        std::cin >> num1;
+
+        if (!calculator.set_num1(num1)) {
+            std::cout << "—Ёб«® ­Ґ ¤®«¦­® Ўлвм а ў­® ­г«о" << std::endl;
+            break;
+        }
+
+        std::cout << "‚ўҐ¤ЁвҐ num2: ";
+        std::cin >> num2;
+
+        if (!calculator.set_num2(num2)) {
+            std::cout << "—Ёб«® ­Ґ ¤®«¦­® Ўлвм а ў­® ­г«о" << std::endl;
+            continue;
+        }
+
+        std::cout << "num1 + num2 = " << calculator.add() << std::endl;
+        std::cout << "num1 - num2 = " << calculator.subtract_1_2() << std::endl;
+        std::cout << "num2 - num1 = " << calculator.subtract_2_1() << std::endl;
+        std::cout << "num1 * num2 = " << calculator.multiply() << std::endl;
+        std::cout << "num1 / num2 = " << calculator.divide_1_2() << std::endl;
+        std::cout << "num2 / num1 = " << calculator.divide_2_1() << std::endl;
+    }
+
+    return 0;
+}
---
+++ task2/task2/task2.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+class Counter {
+private:
+    int count;
+
+public:
+    Counter(int initialCount = 1) : count(initialCount) {}
+
+    void increment() {
+        count++;
+    }
+
+    void decrement() {
+        count--;
+    }
+
+    int getValue() {
+        return count;
+    }
+};
+
+int main() {
+    char command;
+    int initialValue;
+
+    std::cout << "‚ë å®â¨â¥ ãª § âì ­ ç «ì­®¥ §­ ç¥­¨¥ áçñâç¨ª ? ‚¢¥¤¨â¥ ¤  ¨«¨ ­¥â: ";
+    std::string response;
+    std::cin >> response;
+
+    Counter counter; // ¥à¥¬¥é ¥¬ ®¡êï¢«¥­¨¥ counter §  ¯à¥¤¥«ë ¡«®ª  if
+
+    if (response == "¤ ") {
+        std::cout << "‚¢¥¤¨â¥ ­ ç «ì­®¥ §­ ç¥­¨¥ áçñâç¨ª : ";
+        std::cin >> initialValue;
+        counter = Counter(initialValue); // à¨á¢ ¨¢ ¥¬ ­®¢ë© ®¡ê¥ªâ Counter
+    }
+
+    while (true) {
+        std::cout << "‚¢¥¤¨â¥ ª®¬ ­¤ã ('+', '-', '=' ¨«¨ 'x'): ";
+        std::cin >> command;
+
+        if (command == '+') {
+            counter.increment();
+        }
+        else if (command == '-') {
+            counter.decrement();
+        }
+        else if (command == '=') {
+            std::cout << counter.getValue() << std::endl;
+        }
+        else if (command == 'x') {
+            std::cout << "„® á¢¨¤ ­¨ï!" << std::endl;
+            break;
+        }
+        else {
+            std::cout << "¥¢¥à­ ï ª®¬ ­¤ . ®¯à®¡ã©â¥ ¥éñ à §." << std::endl;
+        }
+    }
+
+    return 0;
+}
